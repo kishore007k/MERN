f1b0f780f6029b780c511d41e633f6b1d9e5d9f4
kishore007k <kishorekumarstar07@gmail.com>
2021-01-27 21:34:20 +0530

    Initial commit

---
+++ frontend/src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import ApolloProviderComponent from "./Apollo/ApolloProvider";
+
+ReactDOM.render(
+	<React.StrictMode>
+		<ApolloProviderComponent />
+	</React.StrictMode>,
+	document.getElementById("root")
+);
